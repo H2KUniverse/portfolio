73e33649473bc7d448aacfc6258c6c958ff99f87
Rasmus <pr1nce@live.dk>
2025-02-27 12:47:09 +0100

    Done for now.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaGithub, FaCogs } from 'react-icons/fa'; // Import Github icon from React Icons
+import { FaGithub, FaCogs, FaAddressBook, FaAirbnb, FaAndroid } from 'react-icons/fa'; // Import Github icon from React Icons
 import car1Image from '../src/images/car1.jpg'; // Import the image at the top of your file
 
 
@@ -262,23 +262,55 @@ const Projects = () => {
       icon: FaCogs, // Using React icon from Font Awesome
     },
     {
-      image: 'project2.jpg',
+      image: car1Image,
       headline: 'Project 2',
       description: 'This is a description of Project 2.',
       dateCreated: 'February 2023',
       githubLink: 'https://github.com/yourusername/project2',
-      icon: FaCogs, // Using HTML5 icon from Font Awesome
+      icon: FaAddressBook, // Using HTML5 icon from Font Awesome
     },
     {
-      image: 'project3.jpg',
+      image: car1Image,
       headline: 'Project 3',
       description: 'This is a description of Project 3.',
       dateCreated: 'March 2023',
       githubLink: 'https://github.com/yourusername/project3',
-      icon: FaCogs, // Using C# icon from Font Awesome
+      icon: FaAirbnb, // Using C# icon from Font Awesome
     },
     {
-      image: 'project4.jpg',
+      image: car1Image,
+      headline: 'Project 4',
+      description: 'This is a description of Project 4.',
+      dateCreated: 'April 2023',
+      githubLink: 'https://github.com/yourusername/project4',
+      icon: FaCogs, // Using React icon from Font Awesome
+    },
+    {
+      image: car1Image,
+      headline: 'Project 1',
+      description: 'This is a description of Project 1.',
+      dateCreated: 'January 2023',
+      githubLink: 'https://github.com/yourusername/project1',
+      icon: FaCogs, // Using React icon from Font Awesome
+    },
+    {
+      image: car1Image,
+      headline: 'Project 2',
+      description: 'This is a description of Project 2.',
+      dateCreated: 'February 2023',
+      githubLink: 'https://github.com/yourusername/project2',
+      icon: FaAddressBook, // Using HTML5 icon from Font Awesome
+    },
+    {
+      image: car1Image,
+      headline: 'Project 3',
+      description: 'This is a description of Project 3.',
+      dateCreated: 'March 2023',
+      githubLink: 'https://github.com/yourusername/project3',
+      icon: FaAirbnb, // Using C# icon from Font Awesome
+    },
+    {
+      image: car1Image,
       headline: 'Project 4',
       description: 'This is a description of Project 4.',
       dateCreated: 'April 2023',
@@ -290,7 +322,7 @@ const Projects = () => {
 
   return (
     <section className="projects">
-      <h2>Projects</h2>
+      <h2 className="h2projects">Projects</h2>
       <p>Here are a few of the projects I've worked on...</p>
       <div className="projects-grid">
         {projects.map((project, index) => (
