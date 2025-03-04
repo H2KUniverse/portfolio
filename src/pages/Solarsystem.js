96da92b9dee118a5a5a390b70ad0f212b45d358f
Rasmus <pr1nce@live.dk>
2025-03-04 19:06:15 +0100

    Looks changes.

--- src/pages/Solarsystem.js
+++ src/pages/Solarsystem.js
@@ -469,20 +469,78 @@ const PlanetInfo = () => {
             <label className='closeBig' for='closeSaturn'></label>
             <input className='read' id='closeSaturn' name='saturnRead' type='radio'></input>
             <div className='panel'>
-                <h1>Saturn</h1>
-                <p>Saturn is the sixth planet from the sun and the second largest planet in the solar systm. Saturn was the Roman name for Cronus, the lord of the Titans in Greek mythology. Saturn is the root of the English word "Saturday."</p>
-                <p>Saturn is the farthest planet from Earth visible to the naked human eye, but it is through a telescope that the planet's most outstanding features can be seen: Saturn's rings. Although the other gas giants in the solar systm — Jupiter, Uranus and Neptune — also have rings, those of Saturn are without a doubt the most extraordinary.</p>
-                {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                <img src='http://ak0.picdn.net/shutterstock/videos/4049260/thumb/1.jpg'></img>
-                <h2>Saturn can be seen with the naked eye.</h2>
-                <p>It is the fifth brightest object in the solar systm and is also easily studied through binoculars or a small telescope.</p>
+                <h1>Guides</h1>
+                <p>Saturn is the sixth planet from the sun and the second largest planet in the solar system. Saturn was the Roman name for Cronus, the lord of the Titans in Greek mythology. Saturn is the root of the English word "Saturday."</p>
+                <p>Saturn is the farthest planet from Earth visible to the naked human eye, but it is through a telescope that the planet's most outstanding features can be seen: Saturn's rings. Although the other gas giants in the solar system — Jupiter, Uranus, and Neptune — also have rings, those of Saturn are without a doubt the most extraordinary.</p>
+
+                {/* YouTube Video */}
+                <div className="video-container">
+                    <h2>Remote access to win server through 0-day NSA exploit.</h2>
+                    <hr />
+                    <iframe
+                        width="100%"
+                        height="500px"
+                        src="https://www.youtube.com/embed/sugUOHUIScg"
+                        title="Exploit"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+                <p>Windows server 2012 on VirtualBox.
+                    Kali linux on VirtualBox.
+
+                    This trick? needs a sharefolder to a guest login.
+
+                    Highly recommended for windows server users
+                    to disable the guest login but i suppose many
+                    of you allready done dat. Good job. </p>
+
+                {/* YouTube Video */}
+                <div className="video-container">
+                    <h2>Snapping networks packets to read telnet messages.</h2>
+                    <hr />
+                    <iframe
+                        width="100%"
+                        height="500px"
+                        src="https://www.youtube.com/embed/5YgRdw97FyU"
+                        title="Telnet"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+                <p>Saturn can be seen with the naked eye. It is the fifth brightest object in the solar system and is also easily studied through binoculars or a small telescope.</p>
+
+                {/* YouTube Video */}
+                <div className="video-container">
+                    <h2>Snapping networks packets to read telnet messages.</h2>
+                    <hr />
+                    <iframe
+                        width="100%"
+                        height="500px"
+                        src="https://www.youtube.com/embed/VHM496H397o"
+                        title="Windows Management"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+                <p>Using system.Management to withdraw data from Win32_PnPSignedDriver.
+                    Add reference to your project .
+                    Right click your Reference in Solution explorer, Choose "Add Reference". Find System.Management and add it.
+                    Remember to add using System.Management - In your project too. </p>
+
+
+
                 <h2>Saturn was known to the ancients, including the Babylonians and Far Eastern observers.</h2>
                 <p>It is named for the Roman god Saturnus, and was known to the Greeks as Cronus.</p>
+
                 <h2>Saturn is the flattest planet.</h2>
-                <p>Its polar diameter is 90% of its equatorial diameter, this is due to its low density and fast rotation. Saturn turns on its axis once every 10 hours and 34 minutes giving it the second-shortest day of any of the solar systm’s planets.</p>
+                <p>Its polar diameter is 90% of its equatorial diameter, this is due to its low density and fast rotation. Saturn turns on its axis once every 10 hours and 34 minutes giving it the second-shortest day of any of the solar system’s planets.</p>
+
                 <h2>Saturn orbits the Sun once every 29.4 Earth years.</h2>
                 <p>Its slow movement against the backdrop of stars earned it the nickname of “Lubadsagush” from the ancient Assyrians. The name means “oldest of the old”.</p>
-                <br></br>
             </div>
             <input className='read' id='readUranus' name='uranusRead' type='radio'></input>
             <label className='closeBig' for='closeUranus'></label>
