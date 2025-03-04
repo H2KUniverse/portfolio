8e5113ce05cf70901938aed25d099fae86c47657
Rasmus <pr1nce@live.dk>
2025-03-04 15:33:40 +0100

    Stjerner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SolarSystem from "./pages/Solarsystem";
-import './styles/solarsystem.css'
 
 function App() {
   return (
     <div className="App">
+            <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
       <SolarSystem />
     </div>
   );
